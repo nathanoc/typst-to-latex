5f979431f19d733b639e02070730f88570f46621
nathanoc <72704080+nathanoc@users.noreply.github.com>
2026-09-03 16:46:48 +0100

    added html conversion layer
    The contents of a field are HTML, not plaintext, so passing them directly into pandoc as Typst will not work. Instead we convert from HTML to plaintext first, so that what gets passed into pandoc is (theoretically) identical to what the user believes they typed.

--- src/typst_to_latex/__init__.py
+++ src/typst_to_latex/__init__.py
@@ -16,14 +16,18 @@ def onButtonPress(editor):
     if editor.currentField is None:
         showInfo("You need to select a field")
         return
-
+    
     # Find current field by comparing note fields against editor.currentField
     fields = editor.note.col.models.current()["flds"]
     field_names = [f["name"] for f in fields]
     current_field = field_names[editor.currentField]
-    
-    new_note_text = re.sub("\$(.*?)\$",
-                           lambda match: pypandoc.convert_text(match.group(0), "latex", "typst"),
+
+    # We first convert from HTML to plaintext (which will be valid Typst code), and then convert from Typst to LaTeX.
+    new_note_text = re.sub(r"\$.*?\$",
+                           lambda match: pypandoc.convert_text(
+                               pypandoc.convert_text(match.group(0), "plain", "html"),
+                               "latex",
+                               "typst"),
                            editor.note[current_field])
 
     editor.note[current_field] = new_note_text
